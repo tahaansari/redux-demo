7297f93c4261d4356d39484ba4e63332833a9e20
Taha Ansari <t.ansari@clerkswell.com>
2023-02-08 17:19:47 +0530

    wip

--- user.js
+++ user.js
@@ -1,59 +1,70 @@
 // FETCH USER REQUEST FILE
+const redux = require("redux");
+const axios = require("axios");
 
 // INITIAL GLOBAL STATE
-// const initialState = {
-//   loading: false,
-//   users: [],
-//   error: "",
-// };
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
 
-// const FETCH_USER_REQUEST = "FETCH_USER_REQUEST";
-// const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
-// const FETCH_USER_FAILURE = "FETCH_USER_REQUEST";
+const FETCH_USER_REQUEST = "FETCH_USER_REQUEST";
+const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+const FETCH_USER_FAILURE = "FETCH_USER_REQUEST";
 
-// // ACTION CREATER FUNCTIONS
-// const fetchUserRequest = () => {
-//   return {
-//     type: FETCH_USER_REQUEST,
-//   };
-// };
+// ACTION CREATER FUNCTIONS
+const fetchUserRequest = () => {
+  return {
+    type: FETCH_USER_REQUEST,
+  };
+};
 
-// const fetchUserSuccess = (users) => {
-//   return {
-//     type: FETCH_USER_SUCCESS,
-//     payload: users,
-//   };
-// };
+const fetchUserSuccess = (users) => {
+  return {
+    type: FETCH_USER_SUCCESS,
+    payload: users,
+  };
+};
 
-// const fetchUserFailure = (error) => {
-//   return {
-//     type: FETCH_USER_FAILURE,
-//     payload: error,
-//   };
-// };
+const fetchUserFailure = (error) => {
+  return {
+    type: FETCH_USER_FAILURE,
+    payload: error,
+  };
+};
 
-// const reducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case FETCH_USER_REQUEST:
-//       return {
-//         ...state,
-//         loading: true,
-//       };
-//     case FETCH_USER_SUCCESS:
-//       return {
-//         loading: false,
-//         users: action.payload,
-//         error: "",
-//       };
-//     case FETCH_USER_FAILURE:
-//       return {
-//         loading: false,
-//         users: [],
-//         error: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USER_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case FETCH_USER_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-// const store = redux.createStore(reducer);
+const store = redux.createStore(reducer);
+
+axios
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
